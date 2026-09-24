690714eb05040f15cdb31a24e945ddbcaa92459b
Daniela Lara <dani_menezes.lara@hotmail.com>
2026-09-24 02:00:01 +0000

    inserindo clientes

--- app.py
+++ app.py
@@ -1,4 +1,32 @@
 import streamlit as st
+import psycopg2
+
+conn = psycopg2.connect(
+    host=st.secrets["DB_NAME"],
+    database=st.secrets["DB_NAME"],
+    user=st.secrets["DB_USER"],
+    password=st.secrets["DB_PASSWORD"],
+    port=st.secrets["DB_PORT"]
+)
+
+cursor = conn.cursor()
 
 st.title("Sistema de Cadastro")
 st.write("Bem-vindo ao sistema!")
+
+cursor.execute("SELECT nome, email FRO, cliente")
+dados = cursor.fetchall()
+
+for name, email in dados:
+    st.write(f"{nome} - {email}")
+
+nome = st.text_input("Nome")
+email = st.text_input("Email")
+
+if st.button("Cadastrar"):
+    cursor.execute(
+        "INSERT INTO cliente (nome, cpf, email) VALUES (%s,%s,%s)",
+        (nome, "00000000000", email)
+    )
+    conn.commit()
+    st.success("Cadastrado com sucesso!")
