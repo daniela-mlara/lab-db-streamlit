b64b86ee372ce674f279d475472b00b7979613dc
Daniela Lara <dani_menezes.lara@hotmail.com>
2026-09-24 02:03:14 +0000

    inserindo clientes 2

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import psycopg2
 
 conn = psycopg2.connect(
-    host=st.secrets["DB_NAME"],
+    host=st.secrets["DB_HOST"],
     database=st.secrets["DB_NAME"],
     user=st.secrets["DB_USER"],
     password=st.secrets["DB_PASSWORD"],
